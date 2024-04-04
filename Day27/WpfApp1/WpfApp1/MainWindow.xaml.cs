571d82a4d58224dbd00eadddf5628a0c791b5631
Dio <Dio@DIOZAWARUDO>
2024-04-04 12:28:38 +0300

    Day27, был создан

---
+++ Day27/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+using WpfApp1.models;
+using Microsoft.Extensions.Logging;
+using WpfApp1.Share;
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly XmlDocumentWorker _worker;
+        private readonly ILogger _logger;
+        public bool IsFileOpenned = false;
+        private string _xmlFilePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _logger = LoggerFactory.Create(builder => builder
+            .SetMinimumLevel(LogLevel.Information))
+                .CreateLogger<MainWindow>();
+            _worker = new XmlDocumentWorker(_logger);
+        }
+
+  
+
+      public void PrintCountry(Country country)
+      {
+            textBlockXMlFileContent.Text = "===Counry====" + Environment.NewLine;
+            textBlockXMlFileContent.Text += country?.ToString() ?? "CountryNotFound";
+      }
+
+        private void textBoxDeleteCountryName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCountryName.Text))
+            {
+                PrintContries(_worker.GetAll());
+            }
+        }
+
+        private void PrintContries(List<Country> countries)
+        {
+            textBlockXMlFileContent.Text = "===Counry====" + Environment.NewLine;
+            foreach(var country in countries)
+            {
+                textBlockXMlFileContent.Text += country.ToString();
+            }
+        }
+
+        private void buttonOpenFile_Click_1(object sender, object e)
+        {
+            var dialog = new OpenFileDialog();
+
+            dialog.InitialDirectory = Directory.GetParent(AppContext.BaseDirectory)
+                .Parent
+                .Parent
+                .FullName;
+
+            dialog.DefaultExt = ".xml";
+            dialog.Filter = "Xml documents (.xml)|*.xml";
+            var result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                _xmlFilePath = dialog.FileName;
+                textBlockXMLPathFile.Text = _xmlFilePath;
+                _worker.Load(_xmlFilePath);
+                PrintContries(_worker.GetAll());
+            }
+        }
+
+        private void buttonFindCountryName_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            var counry = _worker.FindBy(textBoxCountryName.Text);
+            PrintCountry(counry);
+        }
+
+        private void buttonAdd_Click_1(object sender, object e)
+        {
+            PrintContries(_worker.GetAll());
+        }
+
+        private void buttonExit_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonDelete_Click_1(object sender, object e)
+        {
+            if (!string.IsNullOrEmpty(textBoxDeleteCountryName.Text) || !string.IsNullOrWhiteSpace(textBoxDeleteCountryName.Text))
+            {
+                _worker.Delete(textBoxDeleteCountryName.Text);
+                PrintContries(_worker.GetAll());
+            }
+        }
+
+      
+    }
+    }
+
+
---
+++ Day27/WpfApp1/WpfApp1/Share/XmlDocumentWorker.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using WpfApp1.models;
+using Microsoft.Extensions.Logging;
+
+
+namespace WpfApp1.Share
+{
+    public class XmlDocumentWorker : IXmlWorker
+    {
+        private readonly XmlDocument _document;
+        private readonly ILogger _logger;
+        private string _xmlFilePath;
+
+        public XmlDocumentWorker(ILogger logger)
+        {
+            _logger = logger;
+            _document = new XmlDocument();
+        }
+
+        public void Add(Country country)
+        {
+            var xRoot = _document.DocumentElement;
+
+            XmlElement countryElem = _document.CreateElement("Страна");
+            XmlAttribute nameAttribute = _document.CreateAttribute("Название");
+            XmlText nameText = _document.CreateTextNode(country.Name);
+            nameAttribute.AppendChild(nameText);
+
+            XmlElement areaElem = _document.CreateElement("Площадь");
+            XmlText areaInnerText = _document.CreateTextNode(country.Area.ToString());
+            areaElem.AppendChild(areaInnerText);
+
+            countryElem.AppendChild(areaElem);
+
+            XmlElement populationElem = _document.CreateElement("Население");
+            XmlText populationInnerText = _document.CreateTextNode(country.Population.ToString());
+            populationElem.AppendChild(populationInnerText);
+
+            countryElem.AppendChild(populationElem);
+
+            xRoot.AppendChild(countryElem);
+            _document.Save(_xmlFilePath);
+        }
+
+        public void Delete(string name)
+        {
+            var xRoot = _document.DocumentElement;
+            foreach(XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    var attributeName = xnode.Attributes.GetNamedItem("Название");
+                    try
+                    {
+                        var attributeNameText = attributeName?.InnerText;
+                        if (attributeNameText.Equals(name))
+                        {
+                            xRoot.RemoveChild(xnode);
+                        }
+                    }
+                    catch (Exception ex) when (ex is XmlException || ex is NullReferenceException)
+                    {
+                        _logger.LogWarning(ex.Message, nameof(attributeName));
+                    }
+                  
+                }
+            }
+        }
+
+        public Country FindBy(string name)
+        {
+            Country country = null;
+            var xRoot = _document.DocumentElement;
+            foreach(XmlNode xmlNode in xRoot)
+            {
+                country = GetCountry(xmlNode);
+                if (country.Name.Equals(name))
+                {
+                    return country;
+                }
+            }
+            return country;
+              
+        }
+
+        public List<Country> GetAll()
+        {
+            List<Country> countries = new List<Country>();
+            var xRoot = _document.DocumentElement;
+            foreach(XmlNode xnode in xRoot)
+            {
+                var country = GetCountry(xnode);
+                countries.Add(country);
+            }
+            return countries;
+
+        }
+        
+        public void Load(string xmlFilePath)
+        {
+            _xmlFilePath = xmlFilePath;
+            _document.Load(xmlFilePath);
+        }
+
+
+        public Country GetCountry(XmlNode node)
+        {
+            var country = new Country();    
+            if(node.Attributes.Count > 0)
+            {
+                var attributeName = node.Attributes.GetNamedItem("Название");
+                country.Name = attributeName?.Value;
+            }
+
+            foreach(XmlNode childNode in node.ChildNodes)
+            {
+                try
+                {
+                    if (childNode.Name.Equals("Название"))
+                    {
+                        country.Name = childNode.InnerText;
+                    }
+                    if (childNode.Name.Equals("Площадь"))
+                    {
+                        country.Area = double.Parse(childNode.InnerText);
+                    }
+                    if (childNode.Name.Equals("Население"))
+                    {
+                        country.Population = double.Parse(childNode.InnerText);
+                    }
+                }
+                catch(Exception ex) when (ex is FormatException || ex is NullReferenceException)
+                {
+                    _logger.LogError(ex.Message, ex.StackTrace, nameof(childNode.InnerText));
+                }
+            }
+            return country;
+        }
+
+    }
+}
